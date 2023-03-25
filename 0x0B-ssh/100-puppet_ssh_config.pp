8b2bcbba88a11c298f24f8c708446f962c39d2fe
callewise <mudemifaustin@gmail.com>
2023-03-25 15:07:25 -0700

    Client configuration file (w/ Puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
