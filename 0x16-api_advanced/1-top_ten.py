ae1bb976e461af38f754be08eb1a82f1c8cc1aa8
callewise <mudemifaustin@gmail.com>
2023-05-09 08:14:06 -0700

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""
-    Queries Reddit API and returns number of subscribers for a given
-    subreddit.
+""" 
+    Queries the Reddit API and prints the titles of the 
+    first 10 hot posts listed for a given subreddit 
 """
 import requests
 
-def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        return int(data["data"]["subscribers"])
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
     else:
-        return 0
-    
+        print(None)
